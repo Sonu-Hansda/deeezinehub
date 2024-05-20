8b03b9fcd4dfa71f6a3dc6f0e16dbd27b3a92755
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-05-20 19:43:13 +0530

    updated home section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,29 +3,29 @@ import { useState } from "react";
 export default function Navbar() {
     const [showMenu, setShowMenu] = useState<boolean>(false);
     return (
-        <nav className="flex justify-between items-center px-8 md:px-24 py-6 border-b">
-            <h1 className="text-center text-3xl md:text-2xl font-bold">DeeezineHub</h1>
+        <nav className="flex justify-between items-center px-8 md:px-24 py-6 bg-secondary">
+            <h1 className="text-center text-3xl font-bold text-primary">DeeezineHub</h1>
 
             {!showMenu ?
                 (
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer md:hidden w-6 h-6" onClick={() => setShowMenu(true)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer md:hidden w-6 h-6 text-gray-300" onClick={() => setShowMenu(true)}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"  />
                     </svg>
                 )
                 :
                 (
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer md:hidden w-6 h-6" onClick={() => setShowMenu(false)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer md:hidden w-6 h-6 text-gray-300" onClick={() => setShowMenu(false)}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                     </svg>
                 )}
 
-            <ul className={`absolute md:relative left-0 top-20 md:top-0 bg-white md:bg-transparent justify-center w-full md:w-auto h-14 md:h-auto ${showMenu? 'flex opacity-100' : 'opacity-0 invisible md:flex'} md:visible md:opacity-100 items-center gap-x-4 text-sm transition-opacity ease-linear`}>
+            <ul className={`absolute md:relative left-0 top-20 md:top-0 bg-primary md:text-gray-300 text-lg md:bg-secondary justify-center w-full md:w-auto h-14 md:h-auto ${showMenu? 'flex opacity-100' : 'opacity-0 invisible md:flex'} md:visible md:opacity-100 items-center gap-x-4 transition-opacity ease-linear`}>
                 <li className="cursor-pointer hover:underline">Home</li>
                 <li className="cursor-pointer hover:underline">Work</li>
                 <li className="cursor-pointer hover:underline">Team</li>
-                <li className="border p-2">contact us</li>
+                <li className="p-2 rounded-lg md:bg-[#3d3e42] md:border md:border-primary">contact us</li>
             </ul>
         </nav>
     );
--- src/sections/Team.tsx
+++ src/sections/Team.tsx
@@ -1,7 +1,15 @@
-export default function Team(){
+export default function Team() {
     return (
-        <section className="p-24 text-center text-5xl bg-[#F5F3F5]">
-            Team Section
+        <section className="p-24 bg-[#F5F3F5]">
+            <h1 className="text-center text-3xl text-slate-600 underline underline-offset-4">Our Team</h1>
+            <ul>
+                <li className="h-24 w-24 p-8 rounded-full flex items-center justify-center bg-gray-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+
+                </li>
+            </ul>
         </section>
     );
 }
