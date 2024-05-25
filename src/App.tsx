a8f8afb642d52b59e65eb5a31bb623e92886b683
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-05-25 15:31:40 +0530

    Added some design with some ui fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Footer from "./sections/Footer"
 import Header from "./sections/Header"
 import Home from "./sections/Home"
+import HowWeWork from "./sections/HowWeWork"
 import Team from "./sections/Team"
 import Work from "./sections/Work"
 
@@ -13,10 +14,15 @@ export default function App() {
           <Home />
         </div>
         <div className="snap-always snap-center" id="page2">
-          <Work />
+        <HowWeWork/>
         </div>
         <div className="snap-always snap-center" id="page3">
+          <Work />
+        </div>
+        <div className="snap-always snap-center" id="page4">
           <Team />
+        </div>
+        <div className="snap-always snap-center" id="page5">
           <Footer />
         </div>
       </main>
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,23 +1,29 @@
 import { FaFacebook, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
-import pic from "../assets/image.png";
+// import pic from "../assets/image.png";
 
 export default function Home() {
     return (
-        <section className="md:px-12 pt-4 md:pt-2 text-white text-5xl text-center bg-secondary">
+        <section className="md:px-12 pt-4 md:pt-2 text-white text-5xl text-center bg-secondary pb-8">
             <div className="flex justify-between ">
 
                 <div className="flex flex-col justify-center w-[50%]">
-                    <p>I'm <span className="text-primary">Abhishek Yadav</span></p>
-                    <p className="text-lg md:text-xl mb-6">Video Editor & Graphics Designer</p>
-                    <button className="border-4 rounded-lg text-lg md:text-3xl p-2 w-1/2 mx-auto border-primary text-primary">Contact me</button>
+                    <p className="text-8xl">DeeZine<span className="text-primary"> Hub</span></p>
+                    <p className="text-lg md:text-xl mb-4 font-thin">Transforming Vision into Reality</p>
+                    <p className="text-lg md:text-xl mb-6 text-justify text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ab aliquid maxime molestiae vero officiis distinctio, mollitia dolor, provident blanditiis expedita libero velit explicabo qui doloribus, est sequi vel quos. Aut, a.</p>
+                    <div className="grid grid-cols-2 text-lg font-light gap-4">
+                        <p className="border p-2 shadow-lg text-primary">250+ Designs</p>
+                        <p className="border p-2 shadow-lg text-primary">50+ Clients</p>
+                        <p className="border p-2 shadow-lg text-primary">150+ Edited Videos</p>
+                        <p className="border p-2 shadow-lg text-primary">25+ Websites</p>
+                    </div>
                 </div>
-                <img className="w-[50%]" src={pic} alt="Profile Image" />
-                <ul className="hidden md:block place-self-end mb-8 text-3xl">
-                    <div className="h-32 w-1 rounded-full mb-4 bg-primary mx-auto"></div>
-                    <li className="mb-2"> <FaFacebook /> </li>
-                    <li className="mb-2"> <FaInstagram /> </li>
-                    <li className="mb-2"> <FaLinkedin /> </li>
-                    <li className="mb-2"> <FaWhatsapp /> </li>
+                {/* <img className="w-[50%]" src={pic} alt="Profile Image" /> */}
+                <ul className="hidden md:block place-self-center mb-8 text-3xl">
+                    {/* <div className="h-32 w-1 rounded-full mb-4 bg-primary mx-auto"></div> */}
+                    <li className="mb-4"> <FaFacebook /> </li>
+                    <li className="mb-4"> <FaInstagram /> </li>
+                    <li className="mb-4"> <FaLinkedin /> </li>
+                    <li className="mb-4"> <FaWhatsapp /> </li>
                 </ul>
             </div>
         </section>
